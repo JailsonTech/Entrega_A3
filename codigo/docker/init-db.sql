e4e72dcf706a21d3e71701b248b7ddbc6e092b8b
Jailson <jailsonrodriguestech@gmail.com>
2024-11-20 21:44:06 -0300

    ajustes persistência dados

--- codigo/docker/init-db.sql
+++ codigo/docker/init-db.sql
@@ -16,7 +16,7 @@ CREATE TABLE vendedores (
 
 CREATE TABLE produtos (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
+    item VARCHAR(255),
     preco DECIMAL,
     estoque INT
 );
@@ -26,16 +26,21 @@ INSERT INTO clientes (nome, cpf, endereco) VALUES
 ('Jailson', '111.222.333-44', 'Endereço 1'),
 ('Carlos', '555.666.777-88', 'Endereço 2'),
 ('Roberto', '777.888.999-00', 'Endereço 3'),
+('Julia', '245.898.789-08', 'Endereço 3'),
 ('Larissa', '734.848.949-40', 'Endereço 4');
 
 INSERT INTO vendedores (nome, cpf) VALUES
-('Alberto', '111.111.111-11'),
-('Suzana', '222.222.222-22'),
-('Carlos', '333.333.333.33'),
-('Vinícius', '343.343.343.43');
+('Alberto', '157.177.158-61'),
+('Suzana', '272.852.292-26');
 
-INSERT INTO produtos (nome, preco, estoque) VALUES
-('feijão', 10.00, 100),
+INSERT INTO produtos (item, preco, estoque) VALUES
+('feijão', 6.99, 100),
 ('arroz', 4.00, 90),
-('macarrão', 3.50, 75),
-('farinha', 5.00, 95);
+('macarrão', 4.49, 75),
+('farinha', 8.99, 95),
+('sal', 2.79, 58),
+('açúcar', 5.99, 12),
+('vinagre', 9.99, 25),
+('azeite', 28.99, 48),
+('tapioca', 4.99, 36),
+('detergente', 2.29, 66);
